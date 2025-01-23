ce98d409b35291e02b8db760586a5571f822f867
Mickael DP <m.dallepasqualine@gmail.com>
2025-01-23 15:17:14 +0100

    Init project, add base components, and setup routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule} from '@angular/router';
+import { ListComponent } from './reports/list/list.component';
+import { CreateComponent } from './reports/create/create.component';
+import { EditComponent } from './reports/edit/edit.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/reports', pathMatch: 'full' },
+    { path:'reports', component: ListComponent },
+    { path: 'reports/create', component: CreateComponent },
+    { path: 'reports/edit/:id', component: EditComponent }
+
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
