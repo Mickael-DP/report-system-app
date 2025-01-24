cdf7efa23eab9faa474a7644f6d68a4907f5d23d
Mickael DP <m.dallepasqualine@gmail.com>
2025-01-24 11:13:28 +0100

    finish create report + valid form & error

--- src/app/reports/create/create.component.ts
+++ src/app/reports/create/create.component.ts
@@ -1,11 +1,58 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule, 
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
 
+  reportForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.reportForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.maxLength(50)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      birthDate: ['', [Validators.required, this.ageValidator]],
+      gender: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      description: ['', Validators.required],
+      observations: [[]],
+    });
+  }
+
+    ageValidator(control: any) {
+      const today = new Date();
+      const birthDate = new Date(control.value);
+      const age = today.getFullYear() - birthDate.getFullYear();
+      return age <= 100 ? null : { ageInvalid: true };
+    }
+
+    onSubmit() {
+      if (this.reportForm.valid) {
+        console.log('report submit', this.reportForm.value);
+      } else {
+        console.log('report form is invalid');
+      }
+    }
+
+
 }
+
